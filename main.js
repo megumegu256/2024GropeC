b393bf76c8079379fded16b631a756d769c520cd
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-10-09 15:31:24 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let counter = 0;
 
+
+
+
+
 //アニメーション
 function incrementCounter() {
     counter++;
+
     setTimeout(sleep, 10);
     function sleep(){
         requestAnimationFrame(incrementCounter);
