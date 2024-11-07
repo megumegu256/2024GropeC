97b60ed33070e81589bb30c016a8816c006d1975
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-11-07 13:54:37 +0900

    update main.js

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     let score = 0;
-    let timeLeft = 10;
+    let timeLeft = 60;
 
     //タイマー表示
     const countdown = document.createElement('div');
